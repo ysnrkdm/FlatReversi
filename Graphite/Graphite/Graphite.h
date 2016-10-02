0f2c9f451f214cec6c53e3f24ca720c5f1d1bbb4
ysnrkdm <euphonictech@gmail.com>
2016-10-02 12:30:35 +0900

    Add Graphite framework

---
+++ Graphite/Graphite/Graphite.h
@@ -0,0 +1,19 @@
+//
+//  Graphite.h
+//  Graphite
+//
+//  Created by KodamaYoshinori on 2016/09/24.
+//  Copyright © 2016 Yoshinori Kodama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Graphite.
+FOUNDATION_EXPORT double GraphiteVersionNumber;
+
+//! Project version string for Graphite.
+FOUNDATION_EXPORT const unsigned char GraphiteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Graphite/PublicHeader.h>
+
+
