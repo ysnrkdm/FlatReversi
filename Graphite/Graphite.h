57f0577858880de2a2abe9bd2b3eb4b2765b2754
ysnrkdm <euphonictech@gmail.com>
2016-10-03 23:06:22 +0900

    Factor Graphite as Embedded Framework

---
+++ Graphite/Graphite.h
@@ -0,0 +1,19 @@
+//
+//  Graphite.h
+//  Graphite
+//
+//  Created by KodamaYoshinori on 2016/10/03.
+//  Copyright © 2016 Yoshinori Kodama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Graphite.
+FOUNDATION_EXPORT double GraphiteVersionNumber;
+
+//! Project version string for Graphite.
+FOUNDATION_EXPORT const unsigned char GraphiteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Graphite/PublicHeader.h>
+
+
---
+++ Graphite/Graphite/Swift_Bridging_Header.h
@@ -0,0 +1,13 @@
+//
+//  Swift_Bridging_Header.h
+//  Graphite
+//
+//  Created by KodamaYoshinori on 2016/10/02.
+//  Copyright © 2016 Yoshinori Kodama. All rights reserved.
+//
+
+#ifndef Swift_Bridging_Header_h
+#define Swift_Bridging_Header_h
+
+
+#endif /* Swift_Bridging_Header_h */
